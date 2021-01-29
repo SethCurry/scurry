767eb194fb8cd1fecea1b077c527d9e8a22ca534
Seth Curry <seth@scurry.io>
2021-01-29 00:36:14 -0600

    Adding .egg-info to .gitignore

--- python/scurry/cliform.py
+++ python/scurry/cliform.py
@@ -1,20 +1,30 @@
 """Edit forms in the CLI."""
 
+import abc
 import typing
 
 ValueType = typing.TypeVar("ValueType")
 
 
-class Value(typing.Generic[ValueType]):
-    def __init__(self, default: ValueType = None, include_in_marshal: bool = True):
-        self.value: typing.Optional[ValueType] = default
+class Value(abc.ABC, typing.Generic[ValueType]):
+    def __init__(self, default: ValueType = None):
+        self.current_value: typing.Optional[ValueType] = default
         return
 
-    def validate(self, item: FieldType):
-        return
-
+    @abc.abstractmethod
     def parse(self, item: typing.Any) -> FieldType:
         raise NotImplementedError("not implemented")
+
+    @abc.abstractmethod 
+    def marshal(self) -> typing.Union[str,int,bool,float]:
+        return ""
+    
+    
+    def value(self) -> FieldType:
+        return self.current_value
+    
+    def set_value(self, new_value: FieldType):
+        self.current_value = new_value
 
 
 class String(Field[str]):
@@ -34,6 +44,25 @@ class Integer(Field[int]):
             "expected an integer, or a string that can be converted to integer"
         )
 
+ValidatorType = typing.Callable[[ValueType], None]
+
+class Field(typing.Generic[ValueType]):
+    def __init__(self, value: Value[ValueType], required=False, validators: typing.List[ValidatorType]=[]):
+        self.validators = validators
+        self.required = required
+        self.value = value
+
+    def parse(self, new_value: typing.Any):
+        new_value = self.value.parse(new_value)
+        self.value.parse()
+        return
+    
+    def value(self) -> ValueType:
+        return self.value.value()
+    
+    def validate(self):
+        self.value.validate(self.value.value())
+    
 
 class Form:
     def __init__(self):
